04d2aab18664dcfec86408d5e6e29b82c931008e
mcapurri <m.capurri@gmail.com>
2021-03-31 23:53:09 +0200

    working on auth

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,14 +5,30 @@ import Login from '../../components/Login/Login';
 import Signup from '../../components/Signup/Signup';
 import Profile from '../../components/Profile/Profile';
 const Home = ({ user, setUser }) => {
+    console.log('user', user);
     const [isSignup, setIsSignup] = useState(false);
 
     return (
         <div className={style.Home}>
             <header>
-                <img src="../../../images/sea-img.png" alt="sea-img" />
+                {user && (
+                    <div
+                        style={{
+                            width: '10rem',
+                            height: '10rem',
+                            border: '10px solid #fff',
+                            backgroundColor: 'transparent',
+                            position: 'fixed',
+                            top: '5%',
+                            borderRadius: '80px',
+                        }}
+                    >
+                        <img src={user.avatar} alt="user-avatar" />
+                    </div>
+                )}
+                <img src="assets/sea-img.png" alt="sea-img" />
             </header>
-            {user === '' ? (
+            {!user ? (
                 isSignup ? (
                     <Signup setUser={setUser} setIsSignup={setIsSignup} />
                 ) : (
--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const userSchema = new Schema(
         },
 
         phoneNumber: String,
+        avatar: String,
     },
     {
         timestamps: true,
