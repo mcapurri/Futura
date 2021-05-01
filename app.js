dc1b306c36f9cbdc270fc64bf730df4961f6c302
mcapurri <m.capurri@gmail.com>
2021-05-01 22:42:51 +0200

    re-styling.

--- app.js
+++ app.js
@@ -152,8 +152,6 @@ const io = require('socket.io')(server, {
 const NEW_CHAT_MESSAGE_EVENT = 'newChatMessage';
 
 io.on('connection', (socket) => {
-    console.log(`Client ${socket.id} connected`);
-
     // Join a conversation
     const { name, room } = socket.handshake.query;
     socket.join(room);
@@ -164,10 +162,10 @@ io.on('connection', (socket) => {
         io.in(room).emit(NEW_CHAT_MESSAGE_EVENT, data);
     });
 
-    // Leave the room if the user closes the socket
+    // Leave the room
     socket.on('disconnect', () => {
-        console.log(`Client ${socket.id} disconnected`);
         socket.leave(room);
+        console.log(`${name} left`);
     });
 });
 
