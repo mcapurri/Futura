4d720be89644d52c2ddf94ae2948d24371c2b343
mcapurri <m.capurri@gmail.com>
2021-05-09 15:38:35 +0200

    working on user deposit

--- client/src/pages/UserDeposit/UserDeposit.js
+++ client/src/pages/UserDeposit/UserDeposit.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import style from './UserDeposit.module.css';
 import { useForm } from 'react-hook-form';
 import useInput from '../../utils/useInput';
-import { Form, Button } from 'bootstrap-4-react';
+import { Form, Button, Row, Col } from 'bootstrap-4-react';
 import axios from 'axios';
 
 const UserDeposit = (props) => {
@@ -13,9 +13,13 @@ const UserDeposit = (props) => {
         formState: { errors },
     } = useForm();
 
-    const [deposited, setDeposited] = useInput(0);
+    const [message, setMessage] = useState('');
+    const [depositedGr, setDepositedGr] = useInput('');
+    const [depositedKg, setDepositedKg] = useInput('');
     const [location, setLocation] = useInput('');
     const [locationsList, setLocationsList] = useState([]);
+    const [email, setEmail] = useInput('');
+    const credit = depositedGr + (depositedKg / 100) * 0.1;
 
     useEffect(() => {
         fetchData();
@@ -30,7 +34,6 @@ const UserDeposit = (props) => {
             console.log('locations', locations.data);
 
             const options = locations.data.map((location) => {
-                console.log('location', location);
                 return (
                     <option key={location._id} value={location.name}>
                         {location.name}
@@ -71,36 +74,25 @@ const UserDeposit = (props) => {
             <div className={style.Form}>
                 <Form onSubmit={handleSubmit(onSubmit)} className={style.Form}>
                     <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            padding: '0 10%',
-                            alignItems: 'center',
-                            width: '100%',
-                            height: '100%',
-                        }}
+                    // style={{
+                    //     display: 'flex',
+                    //     flexDirection: 'column',
+                    //     padding: '0 10%',
+                    //     alignItems: 'center',
+                    //     width: '100%',
+                    //     height: '100%',
+                    // }}
                     >
                         <Form.Group
-                            style={{
-                                width: '100%',
-                                margin: '0 5%',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'space-between',
-                            }}
+                        // style={{
+                        //     width: '100%',
+                        //     margin: '0 5%',
+                        //     display: 'flex',
+                        //     flexDirection: 'column',
+                        //     alignItems: 'space-between',
+                        // }}
                         >
-                            {/* <Form.Input
-                                {...register('name', {
-                                    required: true,
-                                    minLength: 3,
-                                })}
-                                type="text"
-                                placeholder="Location name"
-                                value={name}
-                                onChange={setName}
-                            /> */}
-                            {errors.name && <span>This field is required</span>}
-
+                            <label htmlFor="location">Location</label>
                             <Form.Select
                                 {...register('location', {
                                     required: true,
@@ -109,13 +101,76 @@ const UserDeposit = (props) => {
                                 placeholder="Location"
                                 value={location}
                                 onChange={(e) => setLocation(e.target.value)}
-                                // className={style.TimePicker}
+                                className={style.SelectInput}
                             >
+                                <option>--- Select a location ---</option>
                                 {locationsList}
                             </Form.Select>
                             {errors.name && <span>This field is required</span>}
                         </Form.Group>
+                        <Form.Group>
+                            <label htmlFor="email">Email</label>
+                            <Form.Input
+                                {...register('email', {
+                                    required: true,
+                                    pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                                })}
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={setEmail}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <div className={style.Row}>
+                                <div className={style.Col}>
+                                    <label htmlFor="depositedKg">
+                                        Deposit Kg.
+                                    </label>
+                                    <Form.Input
+                                        {...register('depositedKg', {
+                                            required: true,
+                                        })}
+                                        type="text"
+                                        placeholder="Kilograms"
+                                        value={depositedKg}
+                                        onChange={setDepositedKg}
+                                        className={style.Deposit}
+                                    />
+                                </div>
+
+                                <div className={style.Col}>
+                                    <label htmlFor="depositedGr"></label>
+                                    <Form.Input
+                                        {...register('depositedGr', {
+                                            required: true,
+                                        })}
+                                        type="text"
+                                        placeholder="Grams"
+                                        value={depositedGr}
+                                        onChange={setDepositedGr}
+                                        className={style.Deposit}
+                                        style={{ marginTop: '5.5%' }}
+                                    />
+                                </div>
+                            </div>
+                        </Form.Group>
+                        <Form.Group>
+                            <label htmlFor="credit">Credit $</label>
+                            <Form.Input
+                                {...register('credit', {
+                                    required: true,
+                                })}
+                                type="text"
+                                placeholder="Deposit Kg."
+                                value={credit}
+                                // onChange={setDeposited}
+                            />
+                        </Form.Group>
                     </div>
+                    <p style={{ color: '#fff' }}>{message}</p>
+
+                    <Button type="Submit">Add credit</Button>
                 </Form>
             </div>
         </div>
