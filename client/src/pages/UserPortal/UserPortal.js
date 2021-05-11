54cae7535335c1eb0adc2f9ea7909a88be64257e
mcapurri <m.capurri@gmail.com>
2021-05-11 09:52:58 +0200

    working on user deposit

--- client/src/pages/UserPortal/UserPortal.js
+++ client/src/pages/UserPortal/UserPortal.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import style from './UserPortal.module.css';
 import { ImUserPlus } from 'react-icons/im';
-import { Form, Button } from 'bootstrap-4-react';
+import { Form, Button, InputGroup } from 'bootstrap-4-react';
 import axios from '../../utils/axios';
 import useInput from '../../utils/useInput';
+import { useForm } from 'react-hook-form';
 
 const UserPortal = ({ user, ...props }) => {
-    const [transferAmount, setTransferAmount] = useInput(null);
+    const [transferAmount, setTransferAmount] = useInput('');
+    const {
+        register,
+        handleSubmit,
+        // watch,
+        formState: { errors },
+    } = useForm();
 
-    const handleSubmit = async (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        console.log('transferAmount', transferAmount);
         const { data } = await axios.post(
             '/api/deposits/transfer',
             transferAmount
@@ -42,7 +50,8 @@ const UserPortal = ({ user, ...props }) => {
                     className={style.Row}
                     style={{
                         alignItems: 'center',
-                        margin: '5% 0',
+                        marginTop: '5%',
+                        marginBottom: '7%',
                     }}
                 >
                     <h3 style={{ color: '#eee' }}>
@@ -63,7 +72,6 @@ const UserPortal = ({ user, ...props }) => {
                         )}
                     </div>
                 </div>
-                {/* <section> */}
                 <div className={style.Values}>
                     <div className={style.Row}>
                         <label htmlFor="right">Current Balance</label>
@@ -75,7 +83,7 @@ const UserPortal = ({ user, ...props }) => {
                     <div className={style.Row}>
                         <label htmlFor="left">Total Recycled</label>
                         <div className={style.Display}>
-                            {`${user.totalRecycled} Kg.`}
+                            {`${user.totalRecycled} Kg`}
                         </div>
                     </div>
 
@@ -86,19 +94,54 @@ const UserPortal = ({ user, ...props }) => {
                         </div>
                     </div>
                 </div>
-                {/* </section> */}
             </section>
             <section className={style.Inputcase}>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className={style.Row}>
-                        <label htmlFor="amount">Pay out</label>
-                        <Form.Input
-                            className={style.Display}
-                            name={'amount'}
-                            value={transferAmount}
-                            placeholder={'$$$'}
-                        />
+                <Form onSubmit={onSubmit} className={style.Form}>
+                    <Form.Group
+                        className={style.Row}
+                        style={{ alignItems: 'flex-end' }}
+                    >
+                        <label htmlFor="amount">Cash Tranfer</label>
+                        {/* <InputGroup className={style.InputGroup}> */}
+                        {/* <InputGroup.Prepend
+                                style={{
+                                    height: '2rem',
+                                    backgroundColor: 'rgba(255, 215, 0, 0.5)',
+                                }}
+                            >
+                                <InputGroup.Text>$</InputGroup.Text>
+                            </InputGroup.Prepend> */}
+                        <div
+                            style={{ display: 'flex', alignItems: 'flex-end' }}
+                        >
+                            <span
+                                style={{
+                                    marginRight: '5%',
+                                    fontSize: '1.2rem',
+                                }}
+                            >
+                                $
+                            </span>
+                            <Form.Input
+                                {...register('amount', {
+                                    required: false,
+                                    maxLength: 3,
+                                })}
+                                className={style.Display}
+                                value={transferAmount}
+                                placeholder={'$$$'}
+                                onChange={setTransferAmount}
+                            />
+                            <span style={{ marginLeft: '3%' }}>,00</span>
+                        </div>
+
+                        {/* <InputGroup.Append style={{ height: '2rem' }}>
+                            <InputGroup.Text>.00</InputGroup.Text>
+                            </InputGroup.Append> */}
+                        {/* </InputGroup> */}
                     </Form.Group>
+                    {errors.amount && <span>Max withdraw 999 $</span>}
+                    <Button type="Submit">Pay out</Button>
                 </Form>
             </section>
         </div>
