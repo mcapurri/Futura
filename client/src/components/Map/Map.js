7492e6b2500339c8d6fa8c99f0ccaa319dee6c06
Rohit <varsadarohit@gmail.com>
2021-05-17 13:26:35 +0530

    Rohit second

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -16,143 +16,149 @@ const MapBox = ReactMapboxGl({ accessToken: mapboxtoken });
 const MapBoxStyle = "mapbox://styles/mapbox/streets-v11";
 
 const Map = ({ setIsMapPage }) => {
-  const [map, setMap] = useState();
-  const [dropOffs, setDropOffs] = useState([]);
-  const [geojson, setGeojson] = useState({});
-  console.log("geojson", geojson);
-  const [selectedIndex, setSelectedIndex] = useState(null);
+   const [map, setMap] = useState();
+   const [dropOffs, setDropOffs] = useState([]);
+   const [isMoved, setIsMoved] = useState(false);
+   const [geojson, setGeojson] = useState({});
+   console.log("geojson", geojson);
+   const [selectedIndex, setSelectedIndex] = useState(null);
 
-  useEffect(() => {
-    setIsMapPage(true);
-    return () => {
-      setIsMapPage(false);
-    };
-  }, []);
+   useEffect(() => {
+      setIsMapPage(true);
+      return () => {
+         setIsMapPage(false);
+      };
+   }, []);
 
-  const fetchDropOffs = useCallback(async () => {
-    try {
-      const { data } = await axios.get("/api/dropoffs");
+   const fetchDropOffs = useCallback(async () => {
+      try {
+         const { data } = await axios.get("/api/dropoffs");
 
-      setDropOffs(data);
-      const geojsonData = await getGeoJson(data);
-      setGeojson({ type: "geojson", data: geojsonData });
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+         setDropOffs(data);
+         const geojsonData = await getGeoJson(data);
+         setGeojson({ type: "geojson", data: geojsonData });
+      } catch (err) {
+         console.log(err);
+      }
+   }, []);
 
-  const onLoadMap = useCallback((e) => {
-    setMap(e);
-  }, []);
+   const onLoadMap = useCallback((e) => {
+      setMap(e);
+   }, []);
 
-  const openPopup = (index) => {
-    setSelectedIndex(index);
-  };
-  const closePopup = () => {
-    setSelectedIndex(null);
-  };
+   const openPopup = (index) => {
+      setSelectedIndex(index);
+   };
+   const closePopup = () => {
+      setSelectedIndex(null);
+   };
 
-  const displayMarkers = useMemo(
-    () =>
-      geojson.data &&
-      geojson.data.map((el, index) => (
-        <>
-          {/* {console.log('el', el)} */}
-          <Marker
-            key={el.key}
-            coordinates={el.geometry.coordinates}
-            className={style.Marker}
-            onClick={() => openPopup(el.key)}
-          />
-          {console.log(selectedIndex === el.key)}
-          {selectedIndex !== null && selectedIndex == el.key && (
-            <Popup
-              key={index}
-              visible={selectedIndex === el.key}
-              id={selectedIndex}
-              coordinates={el.geometry.coordinates}
-              onClose={closePopup}
-              closeButton={true}
-              closeOnClick={false}
-              style={{
-                borderRadius: "10px",
-                backgroundColor: "rgb(5, 58, 32)",
-              }}
-            >
-              <p style={{ fontWeight: "500" }}>{el.properties.name}</p>{" "}
-              <p>
-                {el.properties.street}, {el.properties.houseNumber} -{" "}
-                {el.properties.zipCode}{" "}
-              </p>{" "}
-              <p>
-                Opening hours: {el?.properties.openingTime} /{" "}
-                {el?.properties.closingTime}{" "}
-              </p>
-            </Popup>
-          )}
-        </>
-      )),
-    [geojson, selectedIndex]
-  );
+   const displayMarkers = useMemo(
+      () =>
+         geojson.data &&
+         geojson.data.map((el, index) => (
+            <>
+               {/* {console.log('el', el)} */}
+               <Marker
+                  key={el.key}
+                  coordinates={el.geometry.coordinates}
+                  className={style.Marker}
+                  onClick={() => openPopup(el.key)}
+               />
+               {console.log(selectedIndex === el.key)}
+               {selectedIndex !== null && selectedIndex === el.key && (
+                  <Popup
+                     key={index}
+                     visible={selectedIndex === el.key}
+                     id={selectedIndex}
+                     coordinates={el.geometry.coordinates}
+                     onClose={closePopup}
+                     closeButton={true}
+                     closeOnClick={false}
+                     style={{
+                        borderRadius: "10px",
+                        backgroundColor: "rgb(5, 58, 32)",
+                     }}
+                  >
+                     <p style={{ fontWeight: "500" }}>{el.properties.name}</p>{" "}
+                     <p>
+                        {el.properties.street}, {el.properties.houseNumber} -{" "}
+                        {el.properties.zipCode}{" "}
+                     </p>{" "}
+                     <p>
+                        Opening hours: {el?.properties.openingTime} /{" "}
+                        {el?.properties.closingTime}{" "}
+                     </p>
+                  </Popup>
+               )}
+            </>
+         )),
+      [geojson, selectedIndex]
+   );
 
-  useEffect(() => {
-    fetchDropOffs();
-  }, [fetchDropOffs]);
+   useEffect(() => {
+      fetchDropOffs();
+   }, [fetchDropOffs]);
 
-  useEffect(() => {
-    map?.addControl(
-      new MapBoxGL.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-        showUserLocation: true,
-      })
-    );
-  }, [map]);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (data) => {
-          const {
-            coords: { longitude: lng, latitude: lat },
-          } = data;
-
-          map?.setCenter([lng, lat]);
-        },
-        (err) => {
-          console.log("err: ", err);
-        }
+   useEffect(() => {
+      map?.addControl(
+         new MapBoxGL.GeolocateControl({
+            positionOptions: {
+               enableHighAccuracy: true,
+            },
+            trackUserLocation: true,
+            showUserLocation: true,
+         })
       );
-    }
-  }, [map]);
+   }, [map]);
 
-  useEffect(() => {
-    const data = getGeoJson(dropOffs);
-    setGeojson({ type: "geojson", data: data.features });
-  }, [dropOffs]);
+   useEffect(() => {
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(
+            (data) => {
+               const {
+                  coords: { longitude: lng, latitude: lat },
+               } = data;
 
-  useEffect(() => {
-    if (dropOffs.length <= 1) return;
+               map?.setCenter([lng, lat]);
+            },
+            (err) => {
+               console.log("err: ", err);
+            }
+         );
+      }
+   }, [map]);
 
-    const bounds = getBounds(dropOffs);
-    // map?.fitBounds(bounds);
-  }, [map, dropOffs]);
+   useEffect(() => {
+      const data = getGeoJson(dropOffs);
+      setGeojson({ type: "geojson", data: data.features });
+   }, [dropOffs]);
 
-  return (
-    // <div style={{ height: "100%", backgroundColor: "red" }}>
-    <MapBox
-      className={style.Container}
-      style={MapBoxStyle}
-      // center={[13.4, 52.52]}
-      // zoom={[15]}
-      onStyleLoad={onLoadMap}
-    >
-      {displayMarkers}
-    </MapBox>
-    // </div>
-  );
+   useEffect(() => {
+      if (dropOffs.length <= 1) return;
+
+      const bounds = getBounds(dropOffs);
+      // map?.fitBounds(bounds);
+   }, [map, dropOffs]);
+
+   return (
+      // <div style={{ height: "100%", backgroundColor: "red" }}>
+      <MapBox
+         className={style.Container}
+         style={MapBoxStyle}
+         center={[13.4, 52.52]}
+         // zoom={[15]}
+         onMove={() => {
+            if (!isMoved) {
+               setIsMoved(true);
+            }
+         }}
+         onStyleLoad={onLoadMap}
+      >
+         {displayMarkers}
+      </MapBox>
+      // </div>
+   );
 };
 
 export default Map;
