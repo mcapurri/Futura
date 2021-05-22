76fe6beae9be7092f63ceb68a8a6bae9a99e079e
mcapurri <m.capurri@gmail.com>
2021-05-22 11:59:58 +0200

    deployment

--- app.js
+++ app.js
@@ -180,6 +180,6 @@ app.use((req, res) => {
 });
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+// require('./error-handling')(app);
 
 module.exports = server;
